a0a77fb0b4771744509b8d2480997076a0e7801e
Jesse Ceja <jvc68@nau.edu>
2025-10-13 14:39:33 -0700

    Ground work
    Ground work done, this is a little bit harder than I had hoped but making good progress.

--- index.js
+++ index.js
@@ -1,17 +1,36 @@
 //Necessary Set Up
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
 
 const app = express();
-
-const port = 3000;
-
-//Array to hold posts later
-let posts = []
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
+
+const db = new pg.Client({
+	user: "postgres",
+	host: "localhost",
+	database: "Blog",
+	password: "lucy1918",
+	port: 5432,
+});
+
+db.connect();
+
+const port = 3000;
+//Array to hold posts later
+let posts = []
+let exists = false
+db.query(`SELECT * FROM blogs`, (err, res) => {
+	if (err) {
+		console.error(err.stack);
+	} else {
+		posts = res.rows;
+	}
+});
+
 
 //Starts the app.
 app.listen(port, () => {
@@ -20,17 +39,61 @@ app.listen(port, () => {
 
 //Homepage
 app.get("/", (req, res) => {
+	res.redirect("/index")
+});
+
+app.get("/index", (req, res) => {
 	res.render("index.ejs", { posts });
 });
+
+app.get("/signUp", (req, res) => {
+	res.render("signUp.ejs", { exists });
+});
+
+app.get("/signIn", (req, res) => {
+	res.render("signIn.ejs", { exists });
+});
+
+app.post("/attemptSignUp", async (req, res) => {
+	//Receive the bodyparsed stuff in proper format
+	const { name, user_id, password } = req.body;
+
+	try {
+		const result = await db.query("SELECT * FROM users WHERE user_id = $1", [
+			user_id,
+		]);
+
+		if (result.rows.length > 0) {
+			let exists = true
+			res.render("signUp.ejs", { exists });
+		} else {
+			const insertion = await db.query(
+				"INSERT INTO users VALUES ($1, $2, $3)",
+				[user_id, password, name]
+			);
+			console.log(result);
+			res.render("index.ejs", { posts });
+		}
+	} catch (err) {
+		console.log(err);
+	}
+})
+
 
 //Posting Capabilities
 app.post("/addPost", (req, res) => {
 	//Receive the bodyparsed stuff in proper format
-	const { name, title, content } = req.body;
+	const { creator_name, title, body, } = req.body;
+
+	const result = db.query(`
+		INSERT INTO blogs (creator_name, title, body, date_created, creator_user_id) 
+		VALUES ($1, $2, $3, $4, $5)`,
+		[creator_name, title, body, new Date().toLocaleString(), 1]);
+
 	//Add to array
-	posts.push({ name, title, content, time: new Date().toLocaleString() });
+	posts.push({ creator_name, title, body, date_created: new Date().toLocaleString() });
 	//reload home page
-	res.redirect("/");
+	res.redirect("/index");
 });
 
 
@@ -48,7 +111,7 @@ app.post("/editPost/:index", (req, res) => {
 	//Set post at index to body.
 	posts[req.params.index] = { name, title, content, time: new Date().toLocaleString() }
 	//Reload home page
-	res.redirect("/");
+	res.redirect("/index");
 });
 
 //Delete post
@@ -56,5 +119,5 @@ app.post("/delete/:index", (req, res) => {
 	//Delete 1 post at given index.
 	posts.splice(req.params.index, 1);
 	//Reload home page
-	res.redirect("/");
+	res.redirect("/index");
 });
