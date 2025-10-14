beaee7a73f67f94b30b632893e06d12f69758858
Jesse Ceja <jvc68@nau.edu>
2025-10-13 19:02:54 -0700

    Bug Fixing and clean up
    Got what I believe to be the last bug, now I'm gonna do some css stuff and call it a day

--- index.js
+++ index.js
@@ -26,7 +26,6 @@ let posts = []
 let message = ""
 let currentUser = ""
 
-console.log(currentUser);
 
 db.query(`SELECT * FROM blogs`, (err, res) => {
 	if (err) {
@@ -62,7 +61,6 @@ app.get("/signIn", (req, res) => {
 });
 
 app.post("/attemptSignUp", async (req, res) => {
-	//Receive the bodyparsed stuff in proper format
 	const { name, user_id, password } = req.body;
 
 	try {
@@ -88,9 +86,7 @@ app.post("/attemptSignUp", async (req, res) => {
 
 
 app.post("/attemptSignIn", async (req, res) => {
-	//Receive the bodyparsed stuff in proper format
 	const { user_id, password } = req.body;
-
 	try {
 		const result = await db.query("SELECT * FROM users WHERE user_id = $1", [
 			user_id,
@@ -117,7 +113,6 @@ app.post("/attemptSignIn", async (req, res) => {
 
 })
 
-//Posting Capabilities
 app.post("/addPost", async (req, res) => {
 	if (currentUser == "") {
 		message = "Not signed in!"
@@ -127,33 +122,29 @@ app.post("/addPost", async (req, res) => {
 
 	const { creator_name, title, body } = req.body;
 
-	const findPrimaryKey = await db.query(`SELECT MAX(blog_id) FROM blogs`);
+	let findPrimaryKey = await db.query(`SELECT MAX(blog_id) FROM blogs`);
 	let blog_id = findPrimaryKey.rows[0].max
-
+	if (blog_id == null) {
+		await db.query('ALTER SEQUENCE blogs_blog_id_seq RESTART WITH 1')
+	}
 	const result = db.query(`
 		INSERT INTO blogs (creator_name, title, body, date_created, creator_user_id) 
 		VALUES ($1, $2, $3, $4, $5)`,
 		[creator_name, title, body, new Date().toLocaleString(), currentUser]);
 
-
-
-	//Add to array
+	findPrimaryKey = await db.query(`SELECT MAX(blog_id) FROM blogs`);
+	blog_id = findPrimaryKey.rows[0].max
 	posts.push({ blog_id, creator_name, title, body, date_created: new Date().toLocaleString() });
-	//reload home page
 	message = ""
 	res.redirect("/index");
 });
 
 
-//Grabbing the post to edit,
 app.get("/editPost/:index", async (req, res) => {
 	const post = posts[req.params.index]
 	let blog_id = post.blog_id;
-	const verifyUser = await db.query(`SELECT * FROM blogs WHERE blog_id = $1`, [blog_id]);
 
-	console.log("Post by: ", verifyUser.rows[0].creator_user_id)
-	console.log("You are: ", currentUser)
-	console.log("Do they Match? ", verifyUser.rows[0].creator_user_id === currentUser)
+	const verifyUser = await db.query(`SELECT * FROM blogs WHERE blog_id = $1`, [blog_id]);
 
 	if (verifyUser.rows[0].creator_user_id === currentUser) {
 		res.render("editPost.ejs", { post, index: req.params.index })
@@ -164,13 +155,12 @@ app.get("/editPost/:index", async (req, res) => {
 
 });
 
-//Posting the edited post
 app.post("/editPost/:index", async (req, res) => {
 	const { creator_name, title, body } = req.body;
 	let blog_id = posts[req.params.index].blog_id
 	posts[req.params.index] = { blog_id, creator_name, title, body, date_created: new Date().toLocaleString() }
 
-	const verifyUser = await db.query(`UPDATE blogs
+	await db.query(`UPDATE blogs
 	SET creator_name = $1, title = $2, body = $3, date_created = $4
 	WHERE blog_id = $5`, [creator_name, title, body, new Date().toLocaleString(), blog_id]);
 
@@ -178,23 +168,14 @@ app.post("/editPost/:index", async (req, res) => {
 	res.redirect("/index");
 });
 
-//Delete post
 app.post("/delete/:index", async (req, res) => {
-
-
 
 	const post = posts[req.params.index]
 	let blog_id = post.blog_id;
 	const verifyUser = await db.query(`SELECT * FROM blogs WHERE blog_id = $1`, [blog_id]);
 
-	console.log("Post by: ", verifyUser.rows[0].creator_user_id)
-	console.log("You are: ", currentUser)
-	console.log("Do they Match? ", verifyUser.rows[0].creator_user_id === currentUser)
-
 	if (verifyUser.rows[0].creator_user_id === currentUser) {
 		posts.splice(req.params.index, 1);
-
-		//Reload home page
 		message = ""
 		res.redirect("/index");
 		await db.query(`DELETE FROM blogs WHERE blog_id = $1`, [blog_id])
@@ -203,6 +184,5 @@ app.post("/delete/:index", async (req, res) => {
 		message = "Not yours"
 		res.redirect("/")
 	}
-
 
 });
